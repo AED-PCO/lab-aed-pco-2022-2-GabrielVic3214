5f2882f37fd7b2fe1b988caa65d60c7969d3c3eb
Himalaia2143 <himalaia2143@gmail.com>
2022-10-24 16:33:26 -0300

    Lab07

--- codigo/Lab07/Lab07/Program.cs
+++ codigo/Lab07/Lab07/Program.cs
@@ -1,6 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
 namespace Lab07
 {
-    using System.Diagnostics;
     internal class Program
     {
         static void Main(string[] args)
@@ -28,36 +35,42 @@ namespace Lab07
                     Console.WriteLine("Opção Inválida");
                     break;
             }
+            Console.ReadKey();
         }
-        static void Atv1() 
+        static void Atv1()
         {
+            var sw = new Stopwatch();
+            sw.Start();
             Console.Write("Digite uma frase: ");
             char[] frase = Console.ReadLine().ToCharArray();
-            char[] arquivo = System.IO.File.ReadAllText(@"C:\Users\1389156\Desktop\arq1.txt").ToCharArray();
+            char[] arquivo = System.IO.File.ReadAllText(@"D:\PUC\Laboratório\2 - Semestre\Lab testes\frase.txt").ToCharArray();
             int iguais = 0;
-            for(int i = 0; i< frase.Length; i++)
+            for (int i = 0; i < frase.Length; i++)
             {
                 for (int j = 0; j < arquivo.Length; j++)
                 {
-                    if (frase[i].Equals(arquivo[j])){
+                    if (frase[i].Equals(arquivo[j]))
+                    {
                         iguais++;
                     }
                 }
             }
             int diferente = Math.Abs(arquivo.Length - iguais);
-            Console.WriteLine("Iguais: {0} \nDifrentes: {1}",iguais,diferente);
+            Console.WriteLine("Iguais: {0} \nDifrentes: {1}", iguais, diferente);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds.ToString() + " segundos");
         }
-        static void Atv2() 
+        static void Atv2()
         {
             var sw = new Stopwatch();
             sw.Start();
-            int v = funcRec1(6,0);
+            int v = funcRec1(6, 0);
             sw.Stop();
-            Console.WriteLine("Tempo: {0}s\nValor: {1}", sw.ElapsedTicks.ToString(),v);
+            Console.WriteLine("Tempo: {0} segundos\nValor: {1}", sw.ElapsedMilliseconds.ToString(), v);
         }
         static int funcRec1(int a, int quant)
         {
-            if(a <= 1 || quant >= 3002)
+            if (a <= 1 || quant >= 3002)
             {
                 return 0;
             }
@@ -66,24 +79,32 @@ namespace Lab07
                 return funcRec1((a - 2) + 5, ++quant);
             }
         }
-        static void Atv3() 
+        static void Atv3()
         {
-            char[] entrada = System.IO.File.ReadAllText(@"C:\Users\1389156\Desktop\entrada.txt").ToCharArray();
+            var swt = new Stopwatch();
+            swt.Start();
+            char[] entrada = System.IO.File.ReadAllText(@"D:\PUC\Laboratório\2 - Semestre\Lab testes\entrada.txt").ToCharArray();
             char[] reverse = new char[entrada.Length];
             int indexAux = 0;
-            for(int i = entrada.Length-1;i>= 0; i--)
+            for (int i = entrada.Length - 1; i >= 0; i--)
             {
                 reverse[indexAux++] = entrada[i];
             }
-            StreamWriter sw = new StreamWriter(@"C:\Users\1389156\Desktop\saida.txt");
+            StreamWriter sw = new StreamWriter(@"D:\PUC\Laboratório\2 - Semestre\Lab testes\saida.txt");
             sw.WriteLine(new string(reverse));
             sw.Close();
+            swt.Stop();
+            Console.WriteLine(swt.ElapsedMilliseconds.ToString() + " segundos");
         }
         static void Atv4() { }
-        static void Atv5() 
+        static void Atv5()
         {
+            var sw = new Stopwatch();
+            sw.Start();
             int[] v = new int[7] { 10, 5, 7, 15, 22, 9, 30 };
+            sw.Stop();
             Console.WriteLine("Retorno: " + funcao(6, v));
+            Console.WriteLine(sw.ElapsedMilliseconds.ToString() + " segundos");
         }
         static int funcao(int a, int[] v)
         {
